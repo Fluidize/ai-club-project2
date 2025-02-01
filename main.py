380cfb95158a07590ac9d228b386de83789976e7
coderguy134 <mbxiao1@gmail.com>
2025-02-01 16:15:17 -0600

    cleanup, csv chunked

--- main.py
+++ main.py
@@ -7,14 +7,26 @@ print(os.chdir(r'C:\Users\User\Documents\GitHub\ai-club-project2'))
 
 data = pd.read_csv('train_chunk.csv')
 
-def getDaySales(df):
-    sales = []
-    for x in df.index:
-        tarr = []
-        for y in range(90):
-            for row in data['num_sold'].to_list():
-                tarr.append(row)
-        tarr.append(sales)
-    return sales
+def cleanup(X,column):
+    for i in range(len(column)):
+        data=X[column[i]].tolist()
+    
+        if (type(data[0])!=int):
+            sets=set(data)
+            lib={}
+            count=0
 
-print(getDaySales(data))
+            for j in sets:
+                lib.update({j:count})
+                count+=1
+        
+            for k in range(len(data)):
+                data[k]=lib[data[k]]
+            X[column[i]]=data
+
+    return X
+
+X = data.drop(columns=['num_sold'],inplace=False)
+X = cleanup(X, column=X.columns.tolist())
+
+print(X.head())
